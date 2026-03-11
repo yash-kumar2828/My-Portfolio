4f05687e35d4b5d8c29c1707eb85bca74de69a68
yash kumar <you@example.com>
2026-03-11 14:32:43 +0530

    router file

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Home from './page/Home.jsx'
+import Projects from './page/Projects.jsx'
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App />,
+    children:[
+      {index:true, element:<Home />},
+      {path:'/projects', element:<Projects />}
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  
+  <StrictMode>
+      <RouterProvider router={router}/>
+    </StrictMode>
+  
+
+)
